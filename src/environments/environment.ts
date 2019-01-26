ac9de7266d3f85208b34b93b3f32266ba213c867
Dan Nguyen <audan2009@gmail.com>
2019-01-26 16:42:43 -0600

    af2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
   firebase: {
     apiKey: "AIzaSyB22Puf5mrzaI3eGTeoJPkoTYntTuVw_-k",
     authDomain: "iftogether-2019.firebaseapp.com",
